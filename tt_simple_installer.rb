56e53fbd4b49a22cac1a5083eda665d4b6b997b9
Thomas Thomassen <thomas@thomthom.net>
2012-12-03 13:59:38 +0100

    Updated: Added guard for VirtualStore and better loading of RB/RBS.

--- tt_simple_installer.rb
+++ tt_simple_installer.rb
@@ -10,11 +10,8 @@ require 'sketchup.rb'
 #-------------------------------------------------------------------------------
 
 
-# <hotfix>
-# Need to ensure the parent namespaces exists. Normally TT_Lib2 defines this.
 module TT;end
 module TT::Plugins;end
-# </hotfix>
 module TT::Plugins::SimpleInstaller
   
   
@@ -23,7 +20,11 @@ module TT::Plugins::SimpleInstaller
   # Plugin information
   PLUGIN_ID       = 'TT_SimpleInstaller'.freeze
   PLUGIN_NAME     = 'Simple Installer'.freeze
-  PLUGIN_VERSION  = '1.0.3'.freeze
+  PLUGIN_VERSION  = '1.1.0'.freeze
+  
+  # @since 1.1.0
+  PLATFORM_IS_OSX     = (Object::RUBY_PLATFORM =~ /darwin/i) ? true : false
+  PLATFORM_IS_WINDOWS = !PLATFORM_IS_OSX
   
   
   ### MENU & TOOLBARS ### ------------------------------------------------------
@@ -48,9 +49,9 @@ module TT::Plugins::SimpleInstaller
       :name => PLUGIN_NAME,
       :author => 'thomthom',
       :version => PLUGIN_VERSION.to_s,
-      :date => '09 Feb 12',
+      :date => '03 Dec 12',
       :description => 'Adds menu items for easy installation of RBZ or ZIP packaged plugins.',
-      :link_info => 'http://forums.sketchucation.com/viewtopic.php?f=323&t=42315'
+      :link_info => 'http://sketchucation.com/forums/viewtopic.php?f=323&t=42315'
     }
   end
   
@@ -88,13 +89,12 @@ module TT::Plugins::SimpleInstaller
       message += "\n\nThere is already a version of '#{filename}' installed."
     end
     result = UI.messagebox( message, MB_YESNO )
-    return unless result == 6 # ( MB_YES )
+    return unless result == IDYES
     # Check if file can be created.
     unless File.readable?( file )
       return UI.messagebox( "Could not read source file '#{filename}'. Make sure it is located under a path that only contains ASCII characters." )
     end
     # Copy to plugins folder.
-    # (?) No copy method in the standard Ruby lib???
     file_content = nil
     # Read Source
     begin
@@ -112,11 +112,25 @@ module TT::Plugins::SimpleInstaller
     rescue Exception => error
       UI.messagebox( "Error during installation. Could not write destination file.\nError: #{error}" )
     end
-    # Load the plugin.
-    begin
-      Sketchup::require( new_file )
-    rescue
-      UI.messagebox( "Error during installation. Could not load plugin.\nError: #{error}" )
+    # Validate installation. Check for VirtualStore.
+    if self.is_virtualized?( new_file )
+      virtualfile = self.get_virtual_path( file )
+      File.delete( virtualfile )
+      UI.messagebox( "Installation failed. You do not have full permissions to the Plugins folder. Windows tried to place the file in VirtualStore." )
+    else
+      # Load the plugin.
+      # Sketchup::load acts as Sketchup::require and therefore we must remove
+      # the entries from $LOADED_FEATURES in order for it to reload if the file
+      # was updated.
+      $LOADED_FEATURES.delete( new_file )
+      $LOADED_FEATURES.delete( filename )
+      begin
+        unless Sketchup::load( new_file )
+          UI.messagebox( "Could not automatically load plugin." )
+        end
+      rescue
+        UI.messagebox( "Error during installation. Could not load plugin.\nError: #{error}" )
+      end
     end
   end
   
@@ -125,6 +139,38 @@ module TT::Plugins::SimpleInstaller
   # @since 1.0.0
   def self.open_extension_manager
     UI.show_preferences( 'Extensions' )
+  end
+  
+  
+  # @return [String]
+  # @since 1.1.0
+  def self.is_virtualized?( file )
+    if PLATFORM_IS_WINDOWS
+      virtualfile = self.get_virtual_path( file )
+      File.exist?( virtualfile )
+    else
+      false
+    end
+  end
+  
+  
+  # @return [String]
+  # @since 1.1.0
+  def self.get_virtual_path( file )
+    if PLATFORM_IS_WINDOWS
+      filename = File.basename( file )
+      filepath = File.dirname( file )
+      # Verify file exists.
+      unless File.exist?( file )
+        raise ArgumentError, "The file '#{file}' does not exist."
+      end
+      # See if it can be found in virtual store.
+      virtualstore = File.join( ENV['LOCALAPPDATA'], 'VirtualStore' )
+      path = filepath.split(':')[1]
+      File.join( virtualstore, path, filename )
+    else
+      file
+    end
   end
 
   
